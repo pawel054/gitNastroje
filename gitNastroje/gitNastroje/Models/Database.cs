f52d3afe419a30744e4f87c78c8bad3f7aadc221
pawel054 <pawel.marcisz1@gmail.com>
2024-01-31 10:52:16 +0100

    Dodaj klasę bazy danych

--- gitNastroje/gitNastroje/Models/Database.cs
+++ gitNastroje/gitNastroje/Models/Database.cs
@@ -2,11 +2,38 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace gitNastroje.Models
 {
     public class Database
     {
+        readonly SQLiteAsyncConnection _database;
 
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Mood>();
+        }
+
+        public Task<int> InsertMoodAync(Mood mood)
+        {
+            return _database.InsertAsync(mood);
+        }
+
+        public Task<Mood> GetLastMoodAsync()
+        {
+            return _database.Table<Mood>().Where(d => d.Date != DateTime.Now.Date).OrderByDescending(d => d.Date).FirstOrDefaultAsync();
+        }
+
+        public Task<Mood> GetMood(DateTime date)
+        {
+            return _database.Table<Mood>().Where(d => d.Date == date).FirstOrDefaultAsync();
+        }
+
+        public Task<List<Mood>> GetAllMoods()
+        {
+            return _database.Table<Mood>().OrderByDescending(d => d.Date).ToListAsync();
+        }
     }
 }
