07ab13c9362c777154f0ea1aa862714d90412e57
pawel054 <pawel.marcisz1@gmail.com>
2024-01-31 11:24:08 +0100

    Dodaj funkcje włączającą/wyłączającą przyciski

--- gitNastroje/gitNastroje/MainPage.xaml.cs
+++ gitNastroje/gitNastroje/MainPage.xaml.cs
@@ -29,6 +29,41 @@ namespace gitNastroje
             new ImageButton(){ Source = MoodIcons[MoodEnum.okropny] },
         };
 
+        private async void GetLastDayMood()
+        {
+            var result = await App.Database.GetLastMoodAsync();
+
+            if(result != null)
+            {
+                labelLastData.Text = result.Date.ToString("MM/dd/yyyy");
+                labelLastMood.Text = result.EnumMood.ToString();
+            }
+            else
+            {
+                labelLastData.Text = "Brak danych";
+            }
+        }
+
+        private void EnableButtons()
+        {
+            foreach(var btn in Buttons)
+            {
+                btn.IsEnabled = true;
+                btn.BackgroundColor = Color.Transparent;
+            }
+        }
+
+        private void DisableButtons(int btnID)
+        {
+            EnableButtons();
+
+            for(int i=0; i< Buttons.Count; i++)
+            {
+                if(i == btnID)
+                    Buttons[i].BackgroundColor = Color.LightGray;
+                Buttons[i].IsEnabled = false;
+            }
+        }
 
         public MainPage()
         {
