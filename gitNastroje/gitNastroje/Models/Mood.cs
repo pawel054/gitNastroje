b856892a3ccf3e1d423bc2387e4caefefe714285
pawel054 <pawel.marcisz1@gmail.com>
2024-01-31 10:46:13 +0100

    Dodaj enum i klasy

---
+++ gitNastroje/gitNastroje/Models/Mood.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitNastroje.Models
+{
+    public class Mood
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public MoodEnum Enum { get; set; }
+    }
+}
---
+++ gitNastroje/gitNastroje/Models/MoodEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitNastroje.Models
+{
+    public enum MoodEnum
+    {
+        zadowolony = 0,
+        dobry = 1,
+        przeciętny = 2,
+        słaby = 3,
+        okropny = 4
+    }
+}
